5feaa8928b80d114054d3528372f1dd7ae21e96d
Mehul <mehulshrivastava1011@gmail.com>
2023-07-24 23:27:27 +0530

    Added cors option for external communication

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: 'http://aws-blog-frontend.s3-website.ap-south-1.amazonaws.com', // Replace with the actual domain of your Angular app
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.enableCors(corsOptions);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3002);
 }
